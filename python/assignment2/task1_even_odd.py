e5376d6d9190f2d0d7f3305af2ea7f12e9d184d4
aminu1hp <aminulhpp@gmail.com>
2025-06-26 13:59:38 +0530

    td python assignment 2

---
+++ python/assignment2/task1_even_odd.py
@@ -0,0 +1,8 @@
+# Takes number input from user and put it into variable
+num_value = int(input("Enter a number: "))
+
+# It calculates if the number is even or odd
+if num_value % 2 == 0:
+    print(num_value, "is an even number.")
+else:
+    print(num_value, "is a odd number.")
---
+++ python/assignment2/task2_sum_loop.py
@@ -0,0 +1,7 @@
+# Counter resets at 0 which is basic value
+counter = 0
+# for loop iterates the value from range which starts from 1 and put the value into i
+for i in range(1,51,1):
+    # so counter gets incremented by the value of i
+    counter += i
+print("The sum of numbers from 1 to 50 is:", counter)
